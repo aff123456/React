94bcf8f37aec70303a281714c2caa6fefe7bfd66
aff123456 <gabri.serratine@gmail.com>
2021-10-06 16:15:37 -0300

    Create Expenses.js

---
+++ app-01-basics/src/components/Expenses.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./ExpenseItem";
+
+const Expenses = props => {
+
+    const { expenses } = props;
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Expenses;
