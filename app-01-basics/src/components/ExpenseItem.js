21953d128d3537ebd1e81bae6c56569bda621892
aff123456 <gabri.serratine@gmail.com>
2021-10-06 16:15:20 -0300

    Update ExpenseItem.js

--- app-01-basics/src/components/ExpenseItem.js
+++ app-01-basics/src/components/ExpenseItem.js
@@ -1,16 +1,20 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem() {
-  return (
-    <div className="expense-item">
-      <div>Date</div>
-      <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
-      </div>
-    </div>
-    // <h2>Expense Item!</h2>
-  );
+const ExpenseItem = props => {
+
+    const { title = 'Car insurance', amount = 199.99, date = new Date(2021, 2, 28) } = props;
+
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={date} />
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+                <div className="expense-item__price">$ {amount}</div>
+            </div>
+        </div>
+        // <h2>Expense Item!</h2>
+    );
 }
 
 export default ExpenseItem;
