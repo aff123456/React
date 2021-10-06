862196dab1daa5f43c9207599864705fd1520e88
aff123456 <gabri.serratine@gmail.com>
2021-10-06 14:55:33 -0300

    Update App.js

--- app-01-basics/src/App.js
+++ app-01-basics/src/App.js
@@ -1,28 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          This is also visible
-        </p>
-        <ExpenseItem></ExpenseItem>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <p>
+    //       This is also visible
+    //     </p>
+    //     <ExpenseItem></ExpenseItem>
+    //   </header>
+    // </div>
+    <div>
+      <h1>Let's get started</h1>
+      <ExpenseItem />
     </div>
   );
 }
