73eea1ba0c27b1b54d2d4287571666cc83b534b3
aff123456 <gabri.serratine@gmail.com>
2021-10-06 16:14:58 -0300

    Create ExpenseDate.js

---
+++ app-01-basics/src/components/ExpenseDate.js
@@ -0,0 +1,20 @@
+import './ExpenseDate.css';
+
+const ExpenseDate = props => {
+
+    const { date } = props;
+
+    const day = date.toLocaleString('pt-br', { day: '2-digit' });
+    const month = date.toLocaleString('pt-br', { month: 'long' }).toUpperCase();
+    const year = date.getFullYear();
+
+    return (
+        <div className="expense-date">
+            <div className="expense-date__day">{day}</div>
+            <div className="expense-date__month">{month}</div>
+            <div className="expense-date__year">{year}</div>
+        </div>
+    );
+}
+
+export default ExpenseDate;
