cba49f2f5baa8b6e371d6109a4844fc9990c2228
aff123456 <gabri.serratine@gmail.com>
2021-10-07 15:06:10 -0300

    components reorganized into subfolders

--- app-01-basics/src/App.js
+++ app-01-basics/src/App.js
@@ -2,7 +2,7 @@
 // import React from 'react';
 // import ExpenseItem from './components/ExpenseItem';
 import './App.css';
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
 
