82f0257711686978226c5092ae34fab0fe9d9c52
aff123456 <gabri.serratine@gmail.com>
2021-10-08 16:01:54 -0300

    nested ExpensesFilter inside Expenses component

--- app-01-basics/src/components/Expenses/Expenses.js
+++ app-01-basics/src/components/Expenses/Expenses.js
@@ -1,6 +1,8 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = props => {
 
@@ -8,8 +10,19 @@ const Expenses = props => {
         return <ExpenseItem key={index} title={expense.title} amount={expense.amount} date={expense.date} />
     });
 
+    const [filterValue, setFilterValue] = useState('');
+
+    const filterHandler = data => {
+        // const filterYear = { data, id: Math.random().toString() };  // not sure if 'id' is needed, since 'data' is not an object
+        const filterYear = data;
+        console.log('Expenses component');
+        console.log(filterYear);
+        setFilterValue(filterYear);
+    }
+
     return (
         <Card className="expenses">
+            <ExpensesFilter onSave={filterHandler} />
             {expenseItemList}
         </Card>
     );
