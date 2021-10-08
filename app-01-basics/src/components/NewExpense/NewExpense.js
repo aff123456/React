af34e65f208c263d63cc6ca2094e412daebab17f
aff123456 <gabri.serratine@gmail.com>
2021-10-08 15:14:29 -0300

    Update NewExpense.js

--- app-01-basics/src/components/NewExpense/NewExpense.js
+++ app-01-basics/src/components/NewExpense/NewExpense.js
@@ -1,11 +1,18 @@
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
+const NewExpense = props => {
+
+    const getFormData = data => {
+        const expenseData = { ...data, id: Math.random().toString() };
+        console.log('This print came from NewExpense');
+        console.dir(expenseData);
+        props.onSave(data);
+    }
 
     return (
         <div className="new-expense">
-            <ExpenseForm />
+            <ExpenseForm onSave={getFormData} />
         </div>
     );
 }
