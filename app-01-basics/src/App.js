99da7a98831390326164171c91fb4730579ee2e6
aff123456 <gabri.serratine@gmail.com>
2021-10-13 17:32:01 -0300

    uses states to dynamically append new data to be rendered

--- app-01-basics/src/App.js
+++ app-01-basics/src/App.js
@@ -4,45 +4,53 @@
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+import { useState } from 'react';
+
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        title: 'Toilet Paper',
+        amount: 94.12,
+        date: new Date(2020, 7, 14),
+    },
+    {
+        id: 'e2',
+        title: 'New TV',
+        amount: 799.49,
+        date: new Date(2021, 2, 12)
+    },
+    {
+        id: 'e3',
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+    },
+    {
+        id: 'e4',
+        title: 'New Desk (Wooden)',
+        amount: 450,
+        date: new Date(2021, 5, 12),
+    },
+    {
+        id: 'e5',
+        title: 'teste',
+        amount: 199.99,
+        date: new Date(2021, 3, 28),
+    },
+];
 
 const App = () => {
 
-    const expenses = [
-        {
-            id: 'e1',
-            title: 'Toilet Paper',
-            amount: 94.12,
-            date: new Date(2020, 7, 14),
-        },
-        {
-            id: 'e2',
-            title: 'New TV',
-            amount: 799.49,
-            date: new Date(2021, 2, 12)
-        },
-        {
-            id: 'e3',
-            title: 'Car Insurance',
-            amount: 294.67,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: 'e4',
-            title: 'New Desk (Wooden)',
-            amount: 450,
-            date: new Date(2021, 5, 12),
-        },
-        {
-            id: 'e5',
-            title: 'teste',
-        }
-    ];
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
     const newExpenseHandler = data => {
-        const expenseData = { ...data, id: Math.random().toString() };
-        console.log('This print came from App.js');
+        const expenseData = { id: Math.random().toString(), ...data };
+        if (parseFloat(expenseData.amount)) expenseData.amount = parseFloat(expenseData.amount);
+        // console.log('This print came from App.js');
         console.dir(expenseData);
-        expenses.push(expenseData);
+        console.dir(expenses);
+        // setExpenses([expenseData, ...expenses]);
+        setExpenses(prevExpenses => [expenseData, ...prevExpenses]);
     }
 
     // return React.createElement(
