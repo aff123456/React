497d1877c37281b9d969984a1144d5642b956339
aff123456 <gabri.serratine@gmail.com>
2021-10-13 17:29:50 -0300

    saving a new item's date will ignore timezones (which would save it incorrectly)

--- app-01-basics/src/components/NewExpense/ExpenseForm.js
+++ app-01-basics/src/components/NewExpense/ExpenseForm.js
@@ -36,7 +36,7 @@ const ExpenseForm = props => {
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
-            date: new Date(enteredDate)
+            date: new Date(`${enteredDate}T00:00:00`)
         }
         props.onSave(expenseData);
         setFormTitle('');
