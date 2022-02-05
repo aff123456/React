d4a5381823c6a2b99dc6187021b5400944901af8
aff123456 <gabri.serratine@gmail.com>
2022-02-05 12:19:32 -0300

    Users component is now wrapped in a generic Card component

--- app-03-project01/src/components/Users/Users.js
+++ app-03-project01/src/components/Users/Users.js
@@ -3,15 +3,18 @@ import React from "react";
 import User from './User';
 
 import classes from './Users.module.css';
+import Card from '../UI/Card';
 
 const Users = props => {
 
   return (
+    <Card>
     <ul className={classes.users}>
       {props.users.length ? props.users.map((user, index) => (
         <User user={user} key={index} />
       )) : 'No users found'}
     </ul>
+    </Card>
   )
 };
 
