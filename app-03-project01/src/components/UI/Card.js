bd767d89a79fbada7d32ada7119331bb0237a9ca
aff123456 <gabri.serratine@gmail.com>
2022-02-05 12:18:02 -0300

    Card component is now properly exported

--- app-03-project01/src/components/UI/Card.js
+++ app-03-project01/src/components/UI/Card.js
@@ -4,8 +4,10 @@ import classes from './Card.module.css';
 
 const Card = props => {
   return (
-    <div className={classes.card}>
+    <div className={`${classes.card} ${props.className}`}>
       {props.children}
     </div>
   );
 };
+
+export default Card;
