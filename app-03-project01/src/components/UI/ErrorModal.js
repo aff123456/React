c357c48ecd5e14a7000f4f55e8c36b16b20d6275
aff123456 <gabri.serratine@gmail.com>
2022-02-07 18:22:10 -0300

    ErrorModal component now uses portals to render closer to the body element, instead of being nested alongside the Panel component

--- app-03-project01/src/components/UI/ErrorModal.js
+++ app-03-project01/src/components/UI/ErrorModal.js
@@ -1,26 +1,49 @@
 import React, { Fragment } from "react";
+import ReactDom from 'react-dom'
 // import Wrapper from "../Helpers/Wrapper";
 import Button from "./Button";
 
 import Card from "./Card";
 import classes from './ErrorModal.module.css';
 
+const Backdrop = props => {
+  return (
+    <div className={classes.backdrop} onClick={props.onConfirm} />
+  );
+}
+
+const ModalOverlay = props => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button type={'button'} text={'Close'} onClick={props.onConfirm} />
+      </footer>
+    </Card>
+  );
+}
+
 const ErrorModal = props => {
 
   return (
     <Fragment>
-      <div className={classes.backdrop} onClick={props.onConfirm}/>
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button type={'button'} text={'Close'} onClick={props.onConfirm} />
-        </footer>
-      </Card>
+      {ReactDom.createPortal(
+        <Backdrop
+          onConfirm={props.onConfirm} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDom.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm} />,
+        document.getElementById('overlay-root')
+      )}
     </Fragment>
   );
 }
