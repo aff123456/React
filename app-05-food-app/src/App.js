c63cbddf3585cfa33a5cfcf2da282cfffa6f420e
aff123456 <gabri.serratine@gmail.com>
2022-02-19 16:44:16 -0300

    created starter files

---
+++ app-05-food-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Header component</h1>
+      <div>IMAGE</div>
+      <h2>Card component</h2>
+      <h4>Modal component</h4>
+      <h3>Card component</h3>
+      <h4>{'> '}AvailableMeals component</h4>
+      <h5>{'>   > '}MealItem component</h5>
+    </div>
+  );
+}
+
+export default App;
